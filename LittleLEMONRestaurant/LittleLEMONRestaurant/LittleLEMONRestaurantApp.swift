67b7f5157a293b632ca778d24078a236fbca33a8
Md Alif Hossain <alif.cse344@gmail.com>
2024-10-18 02:44:00 +0600

    first commit

---
+++ LittleLEMONRestaurant/LittleLEMONRestaurant/LittleLEMONRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLEMONRestaurantApp.swift
+//  LittleLEMONRestaurant
+//
+//  Created by Md Alif Hossain on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLEMONRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
